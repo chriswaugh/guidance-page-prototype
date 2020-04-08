c4e40afed620ab493efd1712a9794bc54f741ce6
Chris Waugh <chris@electricsheep.ltd>
2020-04-08 11:13:56 +0100

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const config = require('./config')
+
+// Add your routes here - above the module.exports line
+
+module.exports = router
